2d2b0aaa45328076b398360dbc836b6f69919fdc
Ian Neubert <ian@ianneubert.com>
2015-03-30 16:54:51 -0700

    Added heka-tutum plugin

--- plugin_loader.cmake
+++ plugin_loader.cmake
@@ -1,2 +1,3 @@
 add_external_plugin(git https://github.com/ianneub/heka-sqs master)
 add_external_plugin(git https://github.com/ianneub/heka-tags master)
+add_external_plugin(git https://github.com/ianneub/heka-tutum master)
